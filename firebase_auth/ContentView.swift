f07aae4f9d39d57126f71db5553d46da1ab6c5d5
Saiprasad Lokhande <lokhandesai07@gmail.com>
2023-12-17 13:56:14 +0530

    Initial Commit

---
+++ firebase_auth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  firebase_auth
+//
+//  Created by Saiprasad lokhande on 17/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ firebase_auth/firebase_authApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_authApp.swift
+//  firebase_auth
+//
+//  Created by Saiprasad lokhande on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct firebase_authApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
