991102f47e45473e17c6008e8009f97d88b6a359
spLokhandeRejolut <saiprasad.l@rejolut.com>
2023-12-21 19:25:59 +0530

    Working with google sign in

---
+++ firebase_auth/SignInWithGoogle/View/GoogleSignInView.swift
@@ -0,0 +1,18 @@
+//
+//  GoogleSignIn.swift
+//  firebase_auth
+//
+//  Created by Saiprasad Lokhande on 21/12/23.
+//
+
+import SwiftUI
+
+struct GoogleSignIn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoogleSignIn()
+}
